dc48cc1c9c9980701ad8bd35e29dc616a99fc4c9
Kevin Brightwell <nava2@live.ca>
2012-03-02 22:49:19 -0500

    Added initial player tab, it doesn't work.

---
+++ src/admin/playertab/PlayerFieldsPanel.java
@@ -0,0 +1,146 @@
+package admin.playertab;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.ImageIcon;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import data.Contestant;
+
+public class PlayerFieldsPanel extends JPanel {
+	
+	// Store external references:
+	private JLabel labelName;
+	// TODO: Refactor to something more obvious?
+	private JLabel labelCastOff;
+	private JLabel labelCastStatus;
+	private JLabel labelTribe;
+	
+	private JLabel labelID;
+	private JTextField tfContID;
+	
+	private JTextField tfFirstName;
+	private JTextField tfLastName;
+	private JComboBox<String> cbTribe;
+	
+	// store internal
+	private GridBagLayout gbFields;
+	private GridBagConstraints gbFieldsConst;
+	
+	
+	public PlayerFieldsPanel(JLabel _labelName, JTextField _tfFirstName, 
+			JTextField _tfLastName, JLabel _labelID, 
+			JTextField _tfContID, JLabel _labelCastOff, 
+			JLabel _labelCastStatus, JLabel _labelTribe, JComboBox<String> _cbTribe) {
+		super();
+		
+		// passed in
+		labelName = _labelName;
+		labelCastOff = _labelCastOff;
+		labelCastStatus = _labelCastStatus;
+		labelTribe = _labelTribe;
+		
+		tfFirstName = _tfFirstName;
+		tfLastName = _tfLastName;
+		cbTribe = _cbTribe;
+		
+		labelID = _labelID;
+		tfContID = _tfContID;
+		
+		gbFields = new GridBagLayout();
+		gbFieldsConst = new GridBagConstraints();
+		
+		setLayout(gbFields);
+		setupGridBag(gbFields, gbFieldsConst);
+	}
+	
+	private void setupGridBag(GridBagLayout gbl, GridBagConstraints gbc) {
+		//gbc.insets = new Insets(5, 5, 5, 5);
+		
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.gridwidth = 1;
+		gbc.gridheight = 1;
+		gbc.weighty = 0.5;
+		gbc.weightx = 0.5;
+		gbc.gridy = 0;
+		
+		// row: [Name Label] [First Name] [Last Name]
+		gbc.gridx = 0;
+		gbc.gridy++;
+		gbc.weightx = 0.25;
+		gbc.insets = new Insets(0, 0, 5, 10);
+		gbc.anchor = GridBagConstraints.LINE_START;
+		add(labelName, gbc);
+		
+		gbc.weightx = 0.5;
+		gbc.gridx = GridBagConstraints.RELATIVE;
+		gbc.anchor = GridBagConstraints.CENTER;
+		gbc.insets = new Insets(0, 0, 5, 5);
+		add(tfFirstName, gbc);
+		
+		gbc.anchor = GridBagConstraints.LINE_END;
+		gbc.insets = new Insets(0, 0, 5, 0);
+		add(tfLastName, gbc);
+		
+		// row: [ID Label] [ID TextEdit]
+		gbc.gridx = 0;
+		gbc.gridy++;
+		gbc.weightx = 0.25;
+		gbc.anchor = GridBagConstraints.LINE_START;
+		gbc.insets = new Insets(0, 0, 5, 10);
+		add(labelID, gbc);
+		
+		gbc.weightx = 0.5;
+		gbc.gridwidth = GridBagConstraints.REMAINDER;
+		gbc.gridx = GridBagConstraints.RELATIVE;
+		gbc.insets = new Insets(0, 0, 5, 0);
+		add(tfContID, gbc);
+		
+		
+		// row: [Date Label] [Active Label]
+		gbc.gridx = 0;
+		gbc.gridwidth = 1;
+		gbc.gridy++;
+		gbc.anchor = GridBagConstraints.LINE_START;
+		gbc.insets = new Insets(0, 0, 5, 10);
+		add(labelCastOff, gbc);
+		
+		gbc.gridx = GridBagConstraints.RELATIVE;
+		gbc.gridwidth = GridBagConstraints.REMAINDER; // finish the row
+		gbc.anchor = GridBagConstraints.LINE_END;
+		gbc.insets = new Insets(0,0,5,0);
+		add(labelCastStatus, gbc);
+		
+		
+		// row: [Combo Label] [Combo Box]
+		gbc.gridx = 0;
+		gbc.gridy++;
+		gbc.gridwidth = 1;
+		gbc.anchor = GridBagConstraints.LINE_START;
+		gbc.insets = new Insets(0, 0, 5, 10);
+		add(labelTribe, gbc);
+		
+		gbc.gridx = GridBagConstraints.RELATIVE;
+		gbc.anchor = GridBagConstraints.LINE_END;
+		gbc.gridwidth = GridBagConstraints.REMAINDER;
+		gbc.insets = new Insets(0, 0, 5, 0);
+		add(cbTribe, gbc);
+	}
+	
+	// TODO: Implement
+	/**
+	 * Sets the editing information to the information stored in the contestant
+	 * @param c 	The contestant to edit
+	 */
+	public void setEditPane(Contestant c) {
+		return;
+	}
+}
---
+++ src/admin/playertab/PlayerPanel.java
@@ -0,0 +1,381 @@
+package admin.playertab;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JFileChooser;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+import javax.swing.UIManager;
+import javax.swing.table.JTableHeader;
+import javax.swing.table.TableCellRenderer;
+
+import sun.awt.HorizBagLayout;
+import sun.awt.VerticalBagLayout;
+
+import data.Contestant;
+import data.GameData;
+
+
+import admin.Main;
+import admin.contestanttab.ContestantTableModel.SortColumnAdapter;
+
+
+public class PlayerPanel extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+	private JButton imgDisplay;
+	private String imgPath;
+
+	private PlayerFieldsPanel paneEditFields;
+	// container for top stuff
+	private JPanel paneTop;
+	
+	private JButton bCastOff;
+	private JButton bSavePlayer;
+	
+	private JLabel labelName;
+	// TODO: Refactor to something more obvious?
+	private JLabel labelCastOff;
+	private JLabel labelCastStatus;
+	private JLabel labelTribe;
+	
+	private JTextField tfFirstName;
+	private JTextField tfLastName;
+	private JComboBox<String> cbTribe;
+	
+	private JTextField tfContID;
+	private JLabel labelID;
+	
+	private static Contestant INACTIVE_CONT = new Contestant();
+	private Contestant activeCon = INACTIVE_CONT;
+
+	private JTable table;
+	private PlayerTableModel tableModel; 
+	private JTableHeader header;
+	
+	private JButton bAddNew;
+	private JButton bDelete;
+	
+	private static String DEFAULT_PICTURE = "res/test/defaultpic.png";
+	
+	
+	public PlayerPanel(){
+		super();
+		
+		//////////////////////////////
+		// Top Panel:
+		//////////////////////////////
+		// TODO: Better Test picture
+		imgDisplay = new JButton();
+		updateContPicture(DEFAULT_PICTURE); //apparently images have to be .png and alphanumeric
+		
+		
+		// Edit fields:
+		labelName = new JLabel("Name:");
+		tfFirstName = new JTextField();
+		tfLastName = new JTextField();
+		
+		labelCastOff = new JLabel("Cast off:");
+		// TODO: FIx the init of this.. :>
+		labelCastStatus = new JLabel("-");
+		
+		labelTribe = new JLabel("Tribe:");
+		cbTribe = new JComboBox<String>(GameData.getCurrentGame().getTribeNames());
+		
+		labelID = new JLabel("ID:");
+		tfContID = new JTextField();
+		
+		// holds all the fields
+		paneEditFields = new PlayerFieldsPanel(labelName, tfFirstName, 
+					tfLastName, labelID, tfContID, labelCastOff, 
+					labelCastStatus, labelTribe, cbTribe);
+		
+		
+		// buttons:
+		bCastOff = new JButton("Cast Off");
+		bSavePlayer = new JButton("Save");
+		
+		//////////////////////////////
+		// Mid
+		//////////////////////////////
+		tableModel = new PlayerTableModel(GameData.getCurrentGame().getAllContestants());
+		table = new JTable(tableModel);
+		header = table.getTableHeader();
+		
+		//////////////////////////////
+		// Bottom
+		//////////////////////////////
+		bAddNew = new JButton("Add");
+		bDelete = new JButton("Delete");
+		
+		// build the two panes
+		//setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+		setLayout(new BorderLayout(5, 5));
+		buildTopPanel();
+		buildTablePanel();
+		buildBottomPanel();
+		//revalidate();
+		
+		buildActions();
+	}
+	
+	/**
+	 * Builds the top panel including all the editable information
+	 */
+	private void buildTopPanel() {
+		JPanel panel = new JPanel();
+		panel.setLayout(new BorderLayout(10, 10));
+		
+		// this does not need to be referenced else where, only for layout
+		JPanel paneButtons = new JPanel();
+		GridLayout bl = new GridLayout(2, 1);
+		paneButtons.setLayout(bl);
+		
+		paneButtons.add(bCastOff);
+		paneButtons.add(bSavePlayer);
+		
+		// add all components on top:
+		panel.add(imgDisplay, BorderLayout.LINE_START);
+		panel.add(paneEditFields, BorderLayout.CENTER);
+		panel.add(paneButtons, BorderLayout.LINE_END);
+		
+		add(panel, BorderLayout.PAGE_START);
+	}
+	
+	/**
+	 * Builds the panel containing the JTable
+	 */
+	private void buildTablePanel() {
+		JPanel panel = new JPanel();
+		
+		// settings:
+		header.setReorderingAllowed(false); // no moving.
+		table.setColumnSelectionAllowed(true);
+		table.setRowSelectionAllowed(true);
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		
+		header.addMouseListener(tableModel.new SortColumnAdapter());
+			
+		TableCellRenderer renderer = new TableCellRenderer() {
+
+			JLabel label = new JLabel();
+			
+			@Override
+	        public JComponent getTableCellRendererComponent(JTable table,
+	                Object value, boolean isSelected, boolean hasFocus,
+	                int row, int column) {
+	            
+				if (table.isRowSelected(row)) {
+					label.setBackground(Color.RED);
+				} else {
+					label.setBackground(UIManager.getColor("Table.background"));
+				}
+				
+				label.setOpaque(true);
+				label.setText("" + value);
+				
+	            return label;
+	        }
+
+	    };
+	    table.setDefaultRenderer(Object.class, renderer);
+	    
+	    JScrollPane scroll = new JScrollPane(table);
+		
+		panel.setLayout(new BorderLayout(5, 5));
+		panel.add(scroll, BorderLayout.CENTER);
+	    
+	    add(panel, BorderLayout.CENTER);
+	}
+	
+	private void buildBottomPanel() {
+		JPanel panel = new JPanel();
+		panel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+		
+		panel.add(bAddNew);
+		panel.add(bDelete);
+		
+		add(panel, BorderLayout.PAGE_END);
+	}
+	
+	/**
+	 * Updates the image displayed to have the path associated, helper method
+	 * <br>
+	 * <b>Note:</b> Pictures must be PNG format.
+	 * @param path Path to new image.
+	 */
+	//apparently images have to be .png and alphanumeric
+	private void updateContPicture(String path) {
+		// don't update if its already correct!
+		if (imgPath == path) {
+			return;
+		}
+		
+		try {
+			Image img = ImageIO.read(new File(path));
+			
+			// scale the image!
+			if (img.getWidth(null) > 200 ||
+					img.getHeight(null) > 200) {
+				img = img.getScaledInstance(200, 200, Image.SCALE_SMOOTH);
+			}
+			
+			// NO IO errors occured if getting here:
+			ImageIcon imgD = new ImageIcon(img);
+			imgDisplay.setIcon(imgD);
+			imgPath = path;
+		} catch (IOException e) {
+			System.out.println("Exception loading image for contestant " +
+					"picture [" + path + "]");
+			imgDisplay.setIcon(null);
+			imgDisplay.setText("Could not load: " + path);
+		}
+		
+	}
+	
+	/**
+	 * gets the current information with the current contestant, will update 
+	 * from the fields associated.
+	 * @return Current contestant loaded
+	 */
+	private Contestant getCurrentContestant() {
+		boolean newCont = false;
+		Contestant x = null;
+		if (activeCon == INACTIVE_CONT) {
+			activeCon = new Contestant();
+			newCont = true;
+		}
+		
+		activeCon.setFirstName(tfFirstName.getText());
+		activeCon.setLastName(tfLastName.getText());
+		activeCon.setTribe((String)cbTribe.getSelectedItem());
+		activeCon.setPicture(imgPath);
+		
+		String id = tfContID.getText();
+		if (newCont) {
+			if (GameData.getCurrentGame().isIDValid(id)) {
+				activeCon.setID(id);
+			} else {
+				id = GameData.getCurrentGame().generateContestantID(activeCon);
+				activeCon.setID(id);
+			}
+		}
+			
+		x = activeCon;
+		activeCon = INACTIVE_CONT;
+		
+		return x;
+	}
+	
+	private void setActiveContestant(Contestant c) {
+		activeCon = c;
+		
+		tfFirstName.setText(c.getFirstName());
+		tfLastName.setText(c.getLastName());
+		
+		if (c.isCastOff()) {
+			labelCastStatus.setText("Week: " + c.getCastDate());
+		} else {
+			labelCastStatus.setText("Active");
+		}
+		
+		cbTribe.setSelectedItem(c.getTribe());
+		
+		updateContPicture(c.getPicture());
+	}
+	
+	private void buildActions() {
+		bSavePlayer.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(!Main.checkString(tfFirstName.getText().trim(), GameData.REGEX_FIRST_NAME) ||
+						!Main.checkString(tfLastName.getText().trim(), GameData.REGEX_LAST_NAME)){
+					JOptionPane.showMessageDialog(null,"Invalid name!(dialog box not permanent)");
+					return;
+				}
+				
+				if(!Main.checkString(tfContID.getText(), GameData.REGEX_CONTEST_ID)){
+					JOptionPane.showMessageDialog(null, 
+							"Invalid ID! (Generating new ID)");
+					//return;
+				}
+				
+				// check if the contestant is active
+				Contestant con = getCurrentContestant();
+				
+				tableModel.updateContestant(con);
+				
+				System.out.println("We here");
+				
+			}
+			
+		});
+		
+		imgDisplay.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser fc = new JFileChooser();
+				int ret = fc.showOpenDialog(null);
+				if(ret==JFileChooser.APPROVE_OPTION){
+					//File f = fc.getSelectedFile();
+					updateContPicture(fc.getSelectedFile().getAbsolutePath());
+				}	
+			}
+			
+		});
+		
+		bCastOff.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// can't cast off someone already off.
+				if (activeCon.isCastOff())
+					return;
+				
+				activeCon.castOff();
+				
+				labelCastStatus.setText("Week " + activeCon.getCastDate());
+				
+				System.out.println("Casting off: " + activeCon.getID());
+			}
+		});
+		
+		table.addMouseListener(new MouseAdapter() {
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {
+		        int row = table.getSelectedRow();
+		        
+		        Contestant c = tableModel.getByRow(row);
+		        
+		        setActiveContestant(c);
+		    }
+		});
+	}
+	
+}
