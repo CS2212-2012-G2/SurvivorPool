2eb7354776458af8788c00eb829dc316d245f7f9
Ramesh Raj <slyos7@gmail.com>
2012-03-10 21:24:29 -0500

    added the regex library from previous commit

---
+++ src/data/DataUtils.java
@@ -0,0 +1,21 @@
+package data;
+
+import data.me.regexp.RE;
+
+public class DataUtils {
+
+	/**
+	 * Checks if string matches pattern.
+	 * @param val The string to check for validity
+	 * @param pattern A regex pattern that has all possible valid values
+	 * @return true if string matches pattern
+	 */
+	public static boolean checkString(String val,String pattern){
+		if(val==null)
+			return false;
+		if(val.length()==0)
+			return false;
+		RE r = new RE(pattern);
+		return r.match(val);                
+	}
+}
