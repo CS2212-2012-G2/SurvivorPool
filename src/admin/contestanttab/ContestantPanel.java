52a0ecc053fcfd550ac19a0b2a893f7da20a547b
Graem Littleton <rowan@vaothia.com>
2012-03-15 12:53:11 -0400

    Admin can no longer add more than the maximum number of contestants.

--- src/admin/contestanttab/ContestantPanel.java
+++ src/admin/contestanttab/ContestantPanel.java
@@ -148,7 +148,7 @@ public class ContestantPanel extends JPanel implements MouseListener, GameDataDe
 		//////////////////////////////
 		// Bottom
 		//////////////////////////////
-		btnAddCont = new JButton("Add");
+		btnAddCont = new JButton("Add New");
 		btnDeleteCont = new JButton("Delete");
 		
 		// build the two panes
@@ -486,6 +486,15 @@ public class ContestantPanel extends JPanel implements MouseListener, GameDataDe
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				GameData g = GameData.getCurrentGame();
+				if(g.getAllContestants().size() == g.getInitialContestants()){
+					JOptionPane.showMessageDialog(null,"There are already the maximum " +
+							                      "number of contestants in the game.  To add another " +
+							                      "you must delete an existing contestant.");
+					return;
+					
+				}
+				
 				if (isNewContestant) {
 					int response = JOptionPane.showConfirmDialog(null,
 							"Would you like to save a new selected " +
