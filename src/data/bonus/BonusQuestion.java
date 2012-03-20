0a37521fc243ebceb15d969141f72ca1349f57b6
Ramesh Raj <slyos7@gmail.com>
2012-03-19 22:17:24 -0400

    bonus backend has been redone and json works

--- src/data/bonus/BonusQuestion.java
+++ src/data/bonus/BonusQuestion.java
@@ -1,5 +1,10 @@
 package data.bonus;
 
+import data.Contestant;
+import data.me.json.JSONArray;
+import data.me.json.JSONException;
+import data.me.json.JSONObject;
+
 
 /**
  * BonusQuestion is the class that will deal with the numerous bonus questions
@@ -11,7 +16,7 @@ package data.bonus;
  */
 
 public class BonusQuestion {
-	enum BONUS_TYPE{
+	public static enum BONUS_TYPE{
 		MULTI,SHORT;
 	};
 	
@@ -20,67 +25,205 @@ public class BonusQuestion {
 	protected String prompt;
 	protected boolean active;
 	protected String answer;
+	protected String[] choices; //TODO: can we somehow implement short answr and mc together without this?
 	
-	public BonusQuestion() {
-		System.out.println("Not implemented");
-	}
-
-	// ----------------- ACCESSOR METHODS ----------------- //
-
+	public static final String FILE_PATH="res/data/bonus.dat";
+	protected static final String KEY_TYPE="type";
+	protected static final String KEY_PROMPT="prompt";
+	protected static final String KEY_ANSWER="answer";
+	protected static final String KEY_ACTIVE="active";
+	protected static final String KEY_CHOICES="mc_choices";//TODO:need a better name for type
+	
+	
 	/**
-	 * getAnswer is to be implemented by both the ShortAnswer and MultipleChoice
-	 * classes. It will return the correct answer to the Admin when a User has
-	 * attempted to answer a question.
+	 * Default constructor for Bonus Question
+	 * @param prompt The question(required)
+	 * @param answer The answer(required)
+	 * @param choices The possible choices(optional, only for MC)
 	 */
-	public String getAnswer() {
-		return this.answer;
+	public BonusQuestion(String prompt, String answer,String[] choices,boolean active){
+		this.prompt  = prompt;
+		this.answer  = answer;
+		this.choices=choices;
+		bonusType = choices==null?BONUS_TYPE.SHORT:BONUS_TYPE.MULTI; 
+		this.active  = active;
+		//TODO: do we need to check if answer is in choices?
 	}
-
-	// I'm also not sure what the difference between the checkAnswer and
-	// getAnswer methods
-	// is. Why just check it when we can get it and skip over that step? Unless
-	// I'm
-	// misinterpreting it.
-
-	// I just wrote String here because I wasn't sure.
-	// This is just a palceholder method afterall.
-
+	
 	/**
-	 * getType returns the type of question that is to be asked, which can be
-	 * either multiple choice of short answer.
-	 * 
-	 * @return this.type
+	 * Only used for fromJsonObject
 	 */
-	public BONUS_TYPE getType() {
-		return this.bonusType;
+	public BonusQuestion(){}
+	
+	/**
+	 * Get the type of question.
+	 * @return
+	 */
+	public BONUS_TYPE getBonusType() {
+		return bonusType;
 	}
 
 	/**
-	 * isActive returns true if the user is still able to answer a specific
-	 * bonus question.
-	 * 
-	 * @return this.active
+	 * Set the bonus type
+	 * @param bonusType
+	 */
+	public void setBonusType(BONUS_TYPE bonusType) {
+		this.bonusType = bonusType;
+	}
+
+	/**
+	 * Get the question prompt
+	 * @return String of prompt
+	 */
+	public String getPrompt() {
+		return prompt;
+	}
+
+	/**
+	 * Set the question prompt 
+	 * @param prompt
+	 */
+	public void setPrompt(String prompt) {
+		this.prompt = prompt;
+	}
+
+	/**
+	 * Returns if the question is still active
+	 * @return
 	 */
 	public boolean isActive() {
 		return active;
 	}
-	
+
 	/**
-	 * Sets the active state to the value passed.
-	 * @param active	The new state of the question.
+	 * Set if question is active
+	 * @param active
 	 */
 	public void setActive(boolean active) {
 		this.active = active;
 	}
-	
-	
+
+
 	/**
-	 * Checks whether an answer is correct against a stored value. Does this 
-	 * case insensitively.
-	 * @param test The answer to test with.
-	 * @return True if correct.
+	 * Get the answer to the question
+	 * @return
 	 */
-	public boolean checkAnswer(String test) {
-		return test.equalsIgnoreCase(this.answer);
-	}	
+	public String getAnswer() {
+		return answer;
+	}
+
+
+	/**
+	 * Set answer to question
+	 * @param answer
+	 */
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+
+	/**
+	 * Get possible choices or null if question is short answre
+	 * @return
+	 */
+	public String[] getChoices() {
+		return choices;
+	}
+
+	/**
+	 * Set choices. CHANGES TYPE TO MULTIPLE CHOICE!
+	 * @param choices
+	 */
+	public void setChoices(String[] choices) {
+		this.choices = choices;
+		bonusType = BONUS_TYPE.MULTI;
+	}
+
+
+	/**
+	 * Converts Contestant object to a json object
+	 * @return a JSON object containing all the data needed
+	 * @throws JSONException
+	 */
+	public JSONObject toJSONObject() throws JSONException {
+		JSONObject obj = new JSONObject();
+		obj.put(KEY_PROMPT,prompt);
+		obj.put(KEY_ANSWER,answer);
+		
+		if(choices!=null){
+			JSONArray sChoice = new JSONArray();
+			for (String c: choices) {
+				if (c != null)
+					sChoice.put(c);
+			}
+			obj.put(KEY_CHOICES,sChoice);
+		}else{
+			obj.put(KEY_CHOICES,null);
+		}
+		
+		obj.put(KEY_ACTIVE,active);
+		return obj;
+	}
+	
+	public void fromJSONObject(JSONObject o) {
+		try {
+			setPrompt(o.getString(KEY_PROMPT));
+			setAnswer(o.getString(KEY_ANSWER));
+			setActive(o.getBoolean(KEY_ACTIVE));
+			
+			JSONArray jChoices =o.getJSONArray(KEY_CHOICES);
+			if(jChoices==null){
+				setChoices(null);
+			}else{
+				String[] choice = new String[jChoices.length()];
+				for(int i =0;i<jChoices.length();i++){
+					choice[i]=jChoices.getString(i);
+				}
+				setChoices(choice);
+			}
+			
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+	}
+
+	
+	public static void main(String[] args) throws JSONException{
+		BonusQuestion b = new BonusQuestion("question","answer",null,true);
+		String shortActive = b.toJSONObject().toString(); 
+		System.out.println(shortActive);
+		
+		b = new BonusQuestion("question","answer",null,false);
+		String shortNotActive = b.toJSONObject().toString(); 
+		System.out.println(shortNotActive);
+		
+		String[] choices = {"one","two","three","answer"};
+		b = new BonusQuestion("question","answer",choices,true);
+		String mcActive = b.toJSONObject().toString(); 
+		System.out.println(mcActive);
+		
+		b = new BonusQuestion("question","answer",choices,false);
+		String mcNotActive = b.toJSONObject().toString(); 
+		System.out.println(mcNotActive);
+		
+		System.out.println("\n\n\nGenerating fromJSONObject");
+		JSONObject o = new JSONObject(shortActive);
+		BonusQuestion bq = new BonusQuestion();
+		bq.fromJSONObject(o);
+		System.out.println(bq.toJSONObject().toString());
+		
+		o = new JSONObject(shortNotActive);
+		bq = new BonusQuestion();
+		bq.fromJSONObject(o);
+		System.out.println(bq.toJSONObject().toString());
+		
+		o = new JSONObject(mcActive);
+		bq = new BonusQuestion();
+		bq.fromJSONObject(o);
+		System.out.println(bq.toJSONObject().toString());
+		
+		o = new JSONObject(mcNotActive);
+		bq = new BonusQuestion();
+		bq.fromJSONObject(o);
+		System.out.println(bq.toJSONObject().toString());
+	}
 }
