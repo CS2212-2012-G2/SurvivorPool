8e8d43d6dec9cf9773e5fb647dda2b265c9609df
Justin McDonald <jmcdo3@uwo.ca>
2012-03-20 03:36:00 -0400

    working on bonuspanel - WORK IN PROGRESS

--- src/admin/BonusPanel.java
+++ src/admin/BonusPanel.java
@@ -1,65 +1,97 @@
 package admin;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.BoxLayout;
+import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
+import javax.swing.JSpinner;
 import javax.swing.JTextField;
+import javax.swing.SpinnerNumberModel;
+
+import data.GameData;
 
 public class BonusPanel extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 
-	JLabel lblBonusInfo = new JLabel("Bonus info.");
-
+	JPanel pnlNewQ = new JPanel();
+	JPanel pnlListQ = new JPanel();
+	JPanel pnlTypeQ = new JPanel();
+	JPanel pnlViewWeek = new JPanel();
+	
+	JTextField txtQuestion = new JTextField("");
 	JRadioButton rbMultChoice = new JRadioButton("Multiple Choice");
 	JRadioButton rbShortAnswer = new JRadioButton("Short Answer");
-	JTextField txtQuestion = new JTextField("");
-
+	JButton btnNext = new JButton("Next");
+	
+	JLabel lblViewWeek = new JLabel("View Week:");
+	
+	SpinnerNumberModel weekModel = new SpinnerNumberModel(1, 1, 1, 1); // default,low,min,step
+	JSpinner spnWeek = new JSpinner(weekModel);
+	
+	JTextField txtAnswer = new JTextField("");
+	JButton btnBack = new JButton("Back");
+	JButton btnSubmit = new JButton("Submit");
+	
 	public BonusPanel() {
 		initPnlBonus();
-		initPnlAnswer();
+		initListeners();
 	}
 
 	private void initPnlBonus() {
-		JPanel pnlBonus = new JPanel();
-		JPanel pnlMC = new JPanel();
-
-		pnlBonus.setLayout(new BorderLayout());
-		pnlMC.setLayout(new BoxLayout(pnlMC, BoxLayout.Y_AXIS));
-
+		this.setLayout(new BorderLayout());
+		pnlNewQ.setLayout(new GridLayout(1, 4, 50, 20));
+		pnlListQ.setLayout(new BorderLayout());
+		pnlTypeQ.setLayout(new BorderLayout());
+		
+		//build question adding panel
+		txtQuestion.setBorder(BorderFactory.createTitledBorder("Question"));
+		txtQuestion.setPreferredSize(new Dimension(300, 100));
+		
 		ButtonGroup group = new ButtonGroup();
 		group.add(rbMultChoice);
 		group.add(rbShortAnswer);
 
 		rbMultChoice.setAlignmentY(Component.CENTER_ALIGNMENT);
 		rbShortAnswer.setAlignmentY(Component.CENTER_ALIGNMENT);
-		pnlMC.add(rbMultChoice);
-		pnlMC.add(rbShortAnswer);
-
-		JLabel lblQuestion = new JLabel("Question: ");
-		txtQuestion.setSize(150, 150);
-
-		pnlBonus.add(pnlMC, BorderLayout.EAST);
-		pnlBonus.add(lblQuestion, BorderLayout.WEST);
-		pnlBonus.add(txtQuestion, BorderLayout.CENTER);
-
-		this.add(pnlBonus);
+		pnlTypeQ.add(rbMultChoice, BorderLayout.WEST);
+		pnlTypeQ.add(rbShortAnswer, BorderLayout.EAST);
+		pnlTypeQ.add(btnNext, BorderLayout.SOUTH);
+		
+		pnlNewQ.add(txtQuestion);
+		pnlNewQ.add(pnlTypeQ);
+		
+		//build listing information panel
+		pnlListQ.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		
+		pnlViewWeek.add(lblViewWeek);
+		pnlViewWeek.add(spnWeek);
+		
+		pnlListQ.add(pnlViewWeek, BorderLayout.NORTH);
+		
+		this.add(pnlNewQ, BorderLayout.NORTH);
+		this.add(pnlListQ, BorderLayout.SOUTH);
 	}
+	
+	private void initListeners(){
+		btnNext.addActionListener(new ActionListener() {
 
-	private void initPnlAnswer() {
-		JPanel pnlAnswer = new JPanel(); // added panel just incase we need to
-											// add anything else
-		JLabel lblAnswer = new JLabel(
-				"Answer something(Couldn't read writing).");
-
-		pnlAnswer.add(lblAnswer);
-		pnlAnswer.setPreferredSize(new Dimension(480, 200));
-		this.add(pnlAnswer);
+			@Override
+			public void actionPerformed(ActionEvent ae) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
 	}
 }
---
+++ src/test/TestStub_CastOffContestantButton.java
@@ -0,0 +1,20 @@
+package test;
+
+import admin.playertab.PlayerPanel;
+
+/*
+ * Test Case: testing if the GUI cast off button works.
+ */
+public class TestStub_CastOffContestantButton {
+
+	private static final Integer NUM_TEST_CONTESTANTS = 6;
+	
+	public static void main(String[] args) {
+		
+		//Instantiate a ContestantPanel GUI object, so that the cast-off button may be tested.
+		PlayerPanel p = new PlayerPanel();
+		
+		
+
+	}
+}
