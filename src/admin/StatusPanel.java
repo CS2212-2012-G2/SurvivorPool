685920642d990f78a57dcb13ce3e4be04dae4bf1
Kevin Brightwell <nava2@live.ca>
2012-03-20 22:32:51 -0400

    Changed how the status panel works, now it will call the error display AFTER all other swing events execute.

--- src/admin/StatusPanel.java
+++ src/admin/StatusPanel.java
@@ -12,6 +12,7 @@ import javax.swing.AbstractAction;
 import javax.swing.Action;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 import javax.swing.Timer;
 
 /**
@@ -63,6 +64,43 @@ public class StatusPanel extends JPanel {
 	}
 
 	/**
+	 * Class used to run the error timer. This is used to force all other 
+	 * events in the GUI to execute before this is called. 
+	 * @author Kevin Brightwell
+	 */
+	private class RunTimer implements Runnable {
+		
+		private JPanel pane;
+		private String txt;
+		
+		public RunTimer(JPanel pane, String txt) {
+			this.pane = pane;
+			this.txt = txt;
+		}
+		
+		@Override
+		public void run() {
+			if (t != null && t.isRunning()) {
+				t.stop();
+			} else {
+				t = new Timer(TIMER_TIME, displayError);
+				t.setRepeats(false);
+			}
+
+			setMsgLabel(txt);
+			msgLabel.setForeground(Color.white);
+			pane.setBackground(Color.red);
+
+			for (Component c : errorComps)
+				if (c != null)
+					c.setBackground(Color.RED);
+
+			t.start();
+		}
+		
+	}
+	
+	/**
 	 * Display an error message. Makes panel red
 	 * 
 	 * @param txt
@@ -72,23 +110,9 @@ public class StatusPanel extends JPanel {
 	 *            needed)
 	 */
 	public void setErrorMsgLabel(String txt, Component... comps) {
-		if (t != null && t.isRunning()) {
-			t.stop();
-		} else {
-			t = new Timer(TIMER_TIME, displayError);
-			t.setRepeats(false);
-		}
-
-		setMsgLabel(txt);
-		msgLabel.setForeground(Color.white);
-		this.setBackground(Color.red);
-
 		errorComps = comps;
-		for (Component c : comps)
-			if (c != null)
-				c.setBackground(Color.RED);
-
-		t.start();
+		
+		SwingUtilities.invokeLater(new RunTimer(this, txt));	
 	}
 
 	private void resetColor() {
